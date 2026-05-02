69b65c319fd18cf51aa0329c579144977cdfd0f5
LelouchVDK <christian.rettrup@gmail.com>
2026-05-02 12:48:39 +0200

    Dice can be rolled

--- Dionysus/Assets/!/Scenes/HC/HCScripts/Roll.cs
+++ Dionysus/Assets/!/Scenes/HC/HCScripts/Roll.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 class Roll : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    Vector3 lastPosition;
+    char rollNum;
+    Collider finalRollCollider;
+    void Awake()
     {
-        Debug.Log($"You have rolled {name[^1]}");
+        rollNum = name[^1];
+        Debug.Log($"Rollnum variable on Awake {rollNum}");
+    }
+    void OnTriggerStay(Collider other)
+    {
+        char currentRoll = name[^1];
+
+        if (lastPosition == transform.position)
+        {
+            //rollNum = '0';
+            Debug.Log($"Rollnum variable in if statement {rollNum}");
+            rollNum = currentRoll;
+            GetComponent<Collider>().enabled = false;
+        }
+        lastPosition = transform.position;
+        Debug.Log($"Number Roll {name[^1]}");
+        Debug.Log($"Current Roll {currentRoll}");
     }
 }
