417f3449a4927c077827c917606a4b985c90cbe9
Emizu <113088577+XxEmilxX@users.noreply.github.com>
2026-05-03 15:09:26 +0200

    Added Thanks For Playing Scene

--- Dionysus/Assets/!/Scripts/MainMenu.cs
+++ Dionysus/Assets/!/Scripts/MainMenu.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-{ 
+{
+    private void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("TutorialScene");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
